d28f32890091f869f5c7de38b733aef83287b57f
jackstrosahl <strosahljack@gmail.com>
2021-04-19 19:02:14 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,42 @@ import {Tab, Tabs, Container, Row, Col, InputGroup} from 'react-bootstrap';
 import fragsrc from 'raw-loader!glslify-loader!../shaders/frag.glsl';
 import vertsrc from 'raw-loader!glslify-loader!../shaders/vert.glsl';
 import updatesrc from 'raw-loader!glslify-loader!../shaders/update.glsl';
+import makeregl from 'regl';
 
+const GL_CANVAS_ID = "gl-container";
 
 export default class Index extends React.Component {
+  constructor() {
+    super();
+    this.glContainer = React.createRef();
+    this.reglTick = null;
+  }
+
+  setupGlCanvas() {
+    const container = this.glContainer.current;
+    // let node = container.getElementById(GL_CANVAS_ID);
+    // if(this.reglTick !== null) this.reglTick.cancel();
+    // if(node !== null) node.remove();
+    // let canvas = document.createElement("canvas");
+    // canvas.width = container.clientWidth;
+    // canvas.height = container.clientHeight;
+    // canvas.id = GL_CANVAS_ID;
+    // container.appendChild(canvas);
+    const regl = makeregl(container);
+  }
+
+  componentDidMount() {
+      this.setupGlCanvas();
+  }
+
   render() {
     return (
       <Flex column className="Full">
         <Tabs defaultActiveKey="out">
           <Tab eventKey="out" title="Output">
             <Flex column className="Full">
-              <Flex grow id="canvas-container">
-                <canvas className="Full" id="canvas-gl"/>
+              <Flex grow ref={this.glContainer}>
+
               </Flex>
               <Flex>
                 {[0,1,2,3,4,5,6,7,8].map((x) => (
